9895c43cb5137053eae4a5b3b82474fe5bd1da83
Aleksey Mandrykin <almandrykin@ya.ru>
2018-12-07 00:26:02 +0300

    Update веб_сервер.cpp

--- веб_сервер.cpp
+++ веб_сервер.cpp
@@ -64,6 +64,7 @@ vector <string> split(const string &s, char delimiter)
 
 string host_option;
 int port_option;
+bool keep_alive;
 
 void parse_request_options(vector <string> lines)
 {
@@ -73,9 +74,13 @@ void parse_request_options(vector <string> lines)
 		if (option.size() < 2)
 			break;
 
+		string value = option[1];
+		if (value[0] == ' ')
+			value = value.erase(0, 1);
+
 		if (option[0] == "\nHost")
 		{
-			host_option = option[1];
+			host_option = value;
 			if (option.size() > 2)
 				port_option = std::atoi(option[2].c_str());
 
@@ -83,21 +88,58 @@ void parse_request_options(vector <string> lines)
 		}
 		else if (option[0] == "\nConnection")
 		{
-			if (option[1] == " keep_alive")
+			if (value == "keep-alive")
 			{
+				keep_alive = true;
 			}
 			else
-				printf("TODO: parse connection type\n");
+				printf("TODO: parse connection type: '%s'\n", value.c_str());
 
 		}
+		else if (option[0] == "\nUser-Agent")
+			printf("User-Agent: '%s'\n", value.c_str());
+		else if (option[0] == "\nAccept")
+			printf("Accept: '%s'\n", value.c_str());
+		else if (option[0] == "\nAccept-Encoding")
+			printf("Accept-Encoding: '%s'\n", value.c_str());
+		else if (option[0] == "\nAccept-Language")
+			printf("Accept-Language: '%s'\n", value.c_str());
+		else if (option[0] == "\nCache-Control")
+			printf("Cache-Control: '%s'\n", value.c_str());
+		else if (option[0] == "\nUpgrade-Insecure-Requests")
+			printf("Upgrade-Insecure-Requests: '%s'\n", value.c_str());
+		else if (option[0] == "\nReferer")
+			printf("Referer: '%s'\n", value.c_str());
+		else if (option[0] == "\nPragma")
+			printf("Pragma: '%s'\n", value.c_str());
 		else
-			printf("02%d: %s:%s\n", i, option[0].c_str(), option[1].c_str());
-
+			printf("Unknown option - %s: %s\n", option[0].c_str(), value.c_str());
 	}
+	puts("\n");
 }
 
 void response_on_get_request(int client, string pagename)
 {
+	if (pagename == "/")
+		pagename = "/index.html";
+
+	string ext = pagename.substr(pagename.find_first_of(".") + 1);
+	string content_type;
+	if (ext == "html" || ext == "htm")
+		content_type = "text/html";
+	else if (ext == "gif")
+		content_type = "image/gif";
+	else if (ext == "png")
+		content_type = "image/png";
+	else if (ext == "jpg")
+		content_type = "image/jpeg";
+	else if (ext == "css")
+		content_type = "text/css";
+	else if (ext == "xml")
+		content_type = "application/xml";
+	else
+		fprintf(stderr, "Cannot detect content type: %s\n", pagename.c_str());
+
 	string html = Get_HTML(pagename);
 
 	std::stringstream len;
@@ -107,7 +149,7 @@ void response_on_get_request(int client, string pagename)
 	http_response += "Date: Wen 27 Nov 2018 17:30:15 GMT\r\n";
 	http_response += "Server: MyCoolServer/0.0.1\r\n";
 	http_response += "Content-Length: " + len.str() + "\r\n";
-	http_response += "Content-Type: text/html\r\n";
+	http_response += "Content-Type: " + content_type + "\r\n";
 	http_response += "Connection: Closed\r\n";
 	http_response += "\r\n";
 	http_response += html;
@@ -117,7 +159,6 @@ void response_on_get_request(int client, string pagename)
 
 void  serve(int client)
 {
-	string	http_response; // , html;
 	char	http_request[4096];
 	int read_size;
 	read_size = recv(client, http_request, sizeof(http_request), 0);
@@ -127,7 +168,6 @@ void  serve(int client)
 		exit(-1);
 	}
 	http_request[read_size] = 0;
-	//	html = Get_HTML("demo.html");
 
 	vector<string> lines = split(http_request, '\r');
 	parse_request_options(lines);
@@ -139,6 +179,10 @@ void  serve(int client)
 		if ( request_type == "GET")
 		{
 			response_on_get_request(client, request_header[1]);
+		}
+		else
+		{
+			printf("Request '%s' not supported\n", request_type.c_str());
 		}
 
 	}
