445ce4039311dc70719c3d0f8131446cda4efcb7
Aleksey Mandrykin <almandrykin@ya.ru>
2018-12-07 04:56:42 +0300

    Update веб_сервер.cpp
    Made it a bit better

--- веб_сервер.cpp
+++ веб_сервер.cpp
@@ -18,37 +18,37 @@
 
 #include <string>
 #include <sstream>
+#include <iostream>
+#include <fstream>
 
 using namespace std;
 
 typedef struct sockaddr_in  address_t;
 typedef struct sockaddr		sockaddr_t;
 
+string programm_path;
+string http_root_folder;
 
-string Get_HTML(string filename)
+string Get_HTML(string filename, int &response_code)
 {
 	string response;
-#if false
-	string fullpath = "/tmp/";
-	fullpath += filename;
-	FILE * page = fopen(fullpath.c_str(), "rt");
-	if (page == NULL)
-		response = "<html><body><h1>Page " + filename + " not found!</h1>\r\n";
+
+	string fullpath = http_root_folder + filename;
+	ifstream input(fullpath);
+	if (input.fail())
+	{
+		response = "<html><body><h1>Web Page " + filename + " not found!</h1>\r\n";
+		response_code = 404;
+	}
 	else
 	{
-		while (!feof(page))
-		{
-			response += fgetc(page);
-		}
-		fclose(page);
+		stringstream sstr;
+		while (input >> sstr.rdbuf());
+		response = sstr.str();
+		response_code = 200;
 	}
-#else	
-	response = "<html><body><h1>Hello, World!</h1><br><p>This is hardcoded static HTML page</p></body></html>\r\n";
-#endif	
 	return response;
 }
-
-
 
 vector <string> split(const string &s, char delimiter)
 {
@@ -61,9 +61,37 @@ vector <string> split(const string &s, char delimiter)
 	return tokens;
 }
 
+	// Some of HTTP session options
+	string host_option;
+	int port_option;
+	bool keep_alive;
+	string cache_control_option;
+	string user_agent;
+	string accept_encoding;
+	string accept_languages;
+	string referer;
+	string upgrade_insecure_requests;
+	vector<string> accept_format_list;
 
-string host_option;
-int port_option;
+	void parse_accepting_formats(string formats)
+	{
+		accept_format_list.clear();
+
+		vector <string> option = split(formats, ';');
+		for (vector <string>::iterator item = option.begin(); item != option.end(); item++)
+		{
+			vector <string> subitems = split(*item, ',');
+			for (vector <string>::iterator subitem = subitems.begin(); subitem != subitems.end(); subitem++)
+			{
+				bool is_format = subitem->find("/") != std::string::npos;
+				if (is_format)
+				{
+					accept_format_list.push_back(*subitem);
+				}
+			}
+		}
+	}
+
 
 void parse_request_options(vector <string> lines)
 {
@@ -73,79 +101,159 @@ void parse_request_options(vector <string> lines)
 		if (option.size() < 2)
 			break;
 
+		string value = option[1];
+		if (value[0] == ' ')
+			value = value.erase(0, 1);
+
 		if (option[0] == "\nHost")
 		{
-			host_option = option[1];
+			host_option = value;
 			if (option.size() > 2)
 				port_option = std::atoi(option[2].c_str());
 
-			printf("Host: '%s:%d'\n", host_option.c_str(), port_option);
+//			printf("Host: '%s:%d'\n", host_option.c_str(), port_option);
 		}
 		else if (option[0] == "\nConnection")
 		{
-			if (option[1] == " keep_alive")
+			if (value == "keep-alive")
 			{
+				printf("Keep alive connection option\n");
+				keep_alive = true;
 			}
 			else
-				printf("TODO: parse connection type\n");
+				printf("TODO: parse connection type: '%s'\n", value.c_str());
 
 		}
+		else if (option[0] == "\nUser-Agent")
+			user_agent = value;
+		else if (option[0] == "\nAccept")
+			parse_accepting_formats(value);
+		else if (option[0] == "\nAccept-Encoding")
+			accept_encoding = value;
+		else if (option[0] == "\nAccept-Language")
+			accept_languages = value;
+		else if (option[0] == "\nCache-Control")
+			cache_control_option = value;
+		else if (option[0] == "\nUpgrade-Insecure-Requests")
+			upgrade_insecure_requests = value;
+		else if (option[0] == "\nReferer")
+		{
+			referer = value;
+			printf("Referer: '%s'\n", value.c_str());
+		}
+		else if (option[0] == "\nPragma")
+			printf("Pragma: '%s'\n", value.c_str());
 		else
-			printf("02%d: %s:%s\n", i, option[0].c_str(), option[1].c_str());
-
+			printf("Unknown option - %s: %s\n", option[0].c_str(), value.c_str());
 	}
+	puts("\n");
 }
 
 void response_on_get_request(int client, string pagename)
 {
-	string html = Get_HTML(pagename);
+	int response_code;
+	string html;
+
+	printf("GET %s\n", pagename.c_str());
+
+	if (pagename == "/")
+		pagename = "/index.html";
+
+	bool binary = true;
+	string ext = pagename.substr(pagename.find_first_of(".") + 1);
+	string content_type;
+	if (ext == "html" || ext == "htm")
+	{
+		content_type = "text/html";
+		binary = false;
+	}
+	else if (ext == "gif")
+		content_type = "image/gif";
+	else if (ext == "png")
+		content_type = "image/png";
+	else if (ext == "jpg")
+		content_type = "image/jpeg";
+	else if (ext == "css")
+	{
+		content_type = "text/css";
+		binary = false;
+	}
+	else if (ext == "xml")
+	{
+		content_type = "application/xml";
+		binary = false;
+	}
+	else if(ext == "ico")
+		content_type = "image/png";
+	else
+	{
+		content_type = "application/octet-stream";
+		fprintf(stderr, "Cannot detect content type: %s\n", pagename.c_str());
+	}
+
+	html = Get_HTML(pagename, response_code);
 
 	std::stringstream len;
 	len << html.length();
 
-	string http_response = "HTTP/1.1 200 OK\r\n";
+	string http_response = "HTTP/1.1 " + std::to_string(response_code) + " OK\r\n";
 	http_response += "Date: Wen 27 Nov 2018 17:30:15 GMT\r\n";
 	http_response += "Server: MyCoolServer/0.0.1\r\n";
 	http_response += "Content-Length: " + len.str() + "\r\n";
-	http_response += "Content-Type: text/html\r\n";
+	http_response += "Content-Type: " + content_type + "\r\n";
 	http_response += "Connection: Closed\r\n";
 	http_response += "\r\n";
-	http_response += html;
 
-	send(client, http_response.c_str(), http_response.length(), 0);
+//	if (!binary)
+	{
+		http_response += html;
+		send(client, http_response.c_str(), http_response.length(), 0);
+	}
+	//else
+	//{
+	//	send(client, http_response.c_str(), http_response.length(), 0);
+	//}
 }
 
 void  serve(int client)
 {
-	string	http_response; // , html;
 	char	http_request[4096];
 	int read_size;
-	read_size = recv(client, http_request, sizeof(http_request), 0);
-	if (read_size < 0)
-	{
-		perror("Unable read socket");
-		exit(-1);
-	}
-	http_request[read_size] = 0;
-	//	html = Get_HTML("demo.html");
 
-	vector<string> lines = split(http_request, '\r');
-	parse_request_options(lines);
-
-	if (lines.size() > 0)
+	do
 	{
-		vector <string> request_header = split(lines[0], ' ');
-		string request_type = request_header[0];
-		if ( request_type == "GET")
+		keep_alive = false;
+		read_size = recv(client, http_request, sizeof(http_request), 0);
+		if (read_size < 0)
 		{
-			response_on_get_request(client, request_header[1]);
+			perror("Unable read socket");
+			exit(-1);
 		}
+		http_request[read_size] = 0;
 
-	}
-	else
-	{
-		fprintf(stdout, "Got empty request\n");
-	}
+		vector<string> lines = split(http_request, '\r');
+		parse_request_options(lines);
+
+		if (lines.size() > 0)
+		{
+			vector <string> request_header = split(lines[0], ' ');
+			string request_type = request_header[0];
+			if (request_type == "GET")
+			{
+				response_on_get_request(client, request_header[1]);
+			}
+			else
+			{
+				printf("Request '%s' not supported\n", request_type.c_str());
+			}
+
+		}
+		else
+		{
+			fprintf(stdout, "Got empty request\n");
+		}
+	} while (false); // keep_alive);
+
 #if ! defined(_WIN32)
 	exit(0);
 #else
@@ -154,11 +262,52 @@ void  serve(int client)
 #endif
 }
 
-int main()
+void log_AcceptTcpConnection(
+	FILE		*	file, 
+	address_t 		client_addr, 
+	address_t 		server_addr)
+{
+	fprintf(file, "TCP Connection from %d.%d.%d.%d:%d to %d.%d.%d.%d:%d\n",
+		((client_addr.sin_addr.s_addr) & 0x00000ff),
+		((client_addr.sin_addr.s_addr >> 8) & 0x00000ff),
+		((client_addr.sin_addr.s_addr >> 16) & 0x00000ff),
+		htons(client_addr.sin_port),
+		(client_addr.sin_addr.s_addr >> 24),
+		((server_addr.sin_addr.s_addr) & 0x00000ff),
+		((server_addr.sin_addr.s_addr >> 8) & 0x00000ff),
+		((server_addr.sin_addr.s_addr >> 16) & 0x00000ff),
+		(server_addr.sin_addr.s_addr >> 24),
+		htons(server_addr.sin_port)
+		);
+}
+
+void Prepare_HTTP_ROOT()
+{
+#if defined (_WIN32)
+	http_root_folder = programm_path.substr(0, programm_path.find_last_of("\\") + 1);
+#else
+	// We use it in classroom
+	http_root_folder = "/home";
+#endif
+}
+
+// all bits set - allow access, otherwise deny any access
+unsigned int CheckRemoteRights(
+	address_t 		client_addr,
+	address_t 		server_addr)
+{
+	// Full access for everybody. You can implement access rules here
+	return (unsigned int) -1;
+}
+
+int main(int argc, char *argv[])
 {
 	int 			client, server;
 	address_t 		client_addr, server_addr;
 	int				size_of_client_address;
+
+	programm_path = argv[0];
+	Prepare_HTTP_ROOT();
 
 #if defined(WIN32)
 	WSADATA ws;
@@ -194,12 +343,18 @@ int main()
 		exit(-1);
 	}
 
-	printf("Server ready to accept connections\n");
+	fprintf(stdout, "Server ready to accept HTTP requests\n");
 
 	while (true)
 	{
 		size_of_client_address = sizeof(client_addr);
 		client = accept(server, (sockaddr_t *)&client_addr, (socklen_t*)&size_of_client_address);
+		if (CheckRemoteRights(client_addr, server_addr) != (unsigned int)-1)
+		{
+			close(client);
+			log_AcceptTcpConnection(stdout, client_addr, server_addr);
+			continue;
+		}
 #if ! defined(_WIN32)
 		if (client < 0)
 		{
@@ -217,13 +372,7 @@ int main()
 			break;
 
 		default:
-			printf("Connection from %d.%d.%d.%d\n",
-				((client_addr.sin_addr.s_addr) & 0x00000ff),
-				((client_addr.sin_addr.s_addr >> 8) & 0x00000ff),
-				((client_addr.sin_addr.s_addr >> 16) & 0x00000ff),
-				(client_addr.sin_addr.s_addr >> 24)
-				);
-
+			log_AcceptTcpConnection(stdout, client_addr, server_addr);
 			close(client);
 			break;
 		}
@@ -233,6 +382,8 @@ int main()
 			fprintf(stderr, "Unable accept connection: %d\n", WSAGetLastError());
 			exit(-1);
 		}
+		
+		log_AcceptTcpConnection(stdout, client_addr, server_addr);
 
 		serve(client);
 #endif
