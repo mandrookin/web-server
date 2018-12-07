4d0ded2e0f507d586dc8285f2e44da3392d4bb67
Aleksey Mandrykin <almandrykin@ya.ru>
2018-12-07 04:59:38 +0300

    Add files via upload
    Microsoft loves stdafx.h

---
+++ stdafx.h
@@ -0,0 +1,10 @@
+#pragma once
+#define  _CRT_SECURE_NO_WARNINGS  1
+
+
+//#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+// TODO: reference additional headers your program requires here
