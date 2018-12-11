c709bf35eef5f4de1f1a9c3d25b215bc0edcaf9d
Aleksey Mandrykin <almandrykin@ya.ru>
2018-12-12 01:42:31 +0300

    Update for Linux
    And possible for OSX and etc

--- stdafx.h
+++ stdafx.h
@@ -1,10 +1,12 @@
 #pragma once
 #define  _CRT_SECURE_NO_WARNINGS  1
 
-
 //#include "targetver.h"
 
 #include <stdio.h>
+#if defined(_WIN32)
 #include <tchar.h>
-
+#else
+#include <stdlib.h>
+#endif
 // TODO: reference additional headers your program requires here
